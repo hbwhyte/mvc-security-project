b756fbcc2afb6b96ad16146a5afcbccc80097182
hbwhyte <heather.b.whyte@gmail.com>
2018-04-03 10:27:37 +0800

    Add base structure

---
+++ src/main/java/security/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package security.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import security.model.User;
+
+@Repository("userRepository")
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+}
